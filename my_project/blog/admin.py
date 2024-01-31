5597492e415cce40281cbbf041aa9e709694a5e4
Ishan Gujarathi <ishan.gujarathi10@gmail.com>
2024-01-31 12:10:18 +0530

    created a Post model, added few attributes fetching the data from the Post Database model created, to showcase on the frontend side. Also registered the post model in admin.py file and did some updation operations from django admin panel, which were reflected on the frontend side as well!

--- my_project/blog/admin.py
+++ my_project/blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+admin.site.register(Post) #registering our Post model on Django Admin site, so that they are visible in Django Admin Panel
+
+ 
