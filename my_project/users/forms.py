70352a3ff738375b0939b432f4397a4e80db2697
Ishan Gujarathi <ishan.gujarathi10@gmail.com>
2024-02-06 11:54:43 +0530

    added the feature for users to update their profile, automatically resizing of uploaded image by user and also displaying user image besided their blog post in blog home page

--- my_project/users/forms.py
+++ my_project/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm): 
 
@@ -10,8 +11,22 @@ class UserRegisterForm(UserCreationForm):
     and adding an email field to it.
     
     """
-    email = forms.EmailField
+    email = forms.EmailField()
 
     class Meta:
         model = User  #whenever this forms validates, it s going to create a new user, 
         fields = ['username', 'email', 'password1', 'password2']  #fields to be shown on the form
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email'] 
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields =['image'] 
